0b0c29f8974c883c2b0cca8b59fb430137939142
sriyegna <sriyegna@gmail.com>
2016-09-07 23:38:33 -0400

    remove hardcoded values

--- gui.py
+++ gui.py
@@ -178,12 +178,6 @@ def runbutton():
                 instances = int(instances)
             except:
                 print("Invalid instances value given")
-                
-        email = "dopoke4584@gmail.com"
-        googpass = "sonicspeed"
-        count = 3
-        captchakey = "34ee7a4cd3121b207f84028304ef2ed9"
-        instances = "2"
         
         print("Creating " + str(count) + " accounts.")
         pikaptcha(username, password, email, googpass, count, captchakey, captchatimeout, outputfile, lines, startnum, proxy, location, av, tos, int(instances))
