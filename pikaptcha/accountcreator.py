0f6d9de8e67e36c3af614aa398670661f170f24f
Skorfius <skorfius@hotmail.com>
2016-12-13 01:54:08 -0800

    Update accountcreator.py

--- pikaptcha/accountcreator.py
+++ pikaptcha/accountcreator.py
@@ -191,8 +191,6 @@ def create_account(username, password, email, birthday, captchakey2, captchatime
         print("Failed to create user: {}".format(username))
         driver.close()
         raise
-    print ("sleeping for 220 seconds")
-    time.sleep(220)
 
     print("Account successfully created.")
     driver.close()
